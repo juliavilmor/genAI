6285bccaee63ad9ae0e01f5c4cbe91fb4ac16728
juliavilmor <juliavilmor@gmail.com>
2025-09-01 15:23:15 +0200

    update scripts

--- tests/analysis_success_rate.py
+++ tests/analysis_success_rate.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the results of the test success rate
-df = pd.read_csv('test_generation_smooth/results_success_rate.csv', index_col=0)
+#df = pd.read_csv('test_generation_dataset_autoreg/results_success_rate_dm256_nh4_ff1024_nl6_expanded.csv', index_col=0)
+df = pd.read_csv('test_generation_new_dataset/results_success_rate_dm256_nh4_ff1024_nl6.csv', index_col=0)
 
 # Compute the mean, median, mode, and standard deviation of the success rate
 mean = df['success_rate'].mean()
@@ -21,25 +22,25 @@ print('Standard Deviation:', std)
 plt.figure()
 sns.histplot(df['success_rate'], bins=20, kde=True)
 plt.xlabel('Success Rate')
-plt.ylabel('Frequency')
+plt.ylabel('Counts')
 plt.title('Histogram of Success Rate')
-plt.savefig('test_generation_smooth/histogram_success_rate.png')
+plt.savefig('test_generation_new_dataset/histogram_success_rate_exp.png')
 
 # Plot the success rate as a function of the weight index
-plt.figure()
+plt.figure(figsize=(10,6), dpi=300)
 sns.boxplot(x='weight_idx', y='success_rate', data=df)
 plt.xlabel('Weight Index')
 plt.ylabel('Success Rate')
 plt.title('Success Rate as a Function of Weight Index')
-plt.savefig('test_generation_smooth/success_rate_vs_weight_index.png')
+plt.savefig('test_generation_new_dataset/success_rate_vs_weight_index_exp.png')
 
 # Plot the histogram of the success rate for each weight index
 plt.figure()
-sns.histplot(df, x='success_rate', hue='weight_idx', bins=20, kde=True)
+sns.histplot(df, x='success_rate', hue='weight_idx', bins=30, element='step', kde=True, linewidth=0)
 plt.xlabel('Success Rate')
-plt.ylabel('Frequency')
+plt.ylabel('Counts')
 plt.title('Histogram of Success Rate for Each Weight Index')
-plt.savefig('test_generation_smooth/histogram_success_rate_vs_weight_index.png')
+plt.savefig('test_generation_new_dataset/histogram_success_rate_vs_weight_index_exp.png')
 
 # Success rate per protein sequence
 plt.figure(figsize=(30,10))
@@ -47,4 +48,4 @@ sns.boxplot(x='seq_idx', y='success_rate', data=df)
 plt.xlabel('Sequence Index')
 plt.ylabel('Success Rate')
 plt.title('Success Rate per Protein Sequence')
-plt.savefig('test_generation_smooth/success_rate_vs_sequence_index.png')
+plt.savefig('test_generation_new_dataset/success_rate_vs_sequence_index_exp.png')
--- tests/test_success_rate.py
+++ tests/test_success_rate.py
@@ -7,21 +7,32 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from generate import generate
 from decoder_model import MultiLayerTransformerDecoder
 from tokenizer import Tokenizer
+import glob
+
 
 # List of protein sequences --> taken from the cluster representatives (top 100 most populated clusters)
 df = pd.read_csv('test_generation/cluster_representatives_100.csv', index_col=0)
 seqs = df['Sequence'].tolist()
 
-weights_files = ['../weights/weights_dm256_nh4_ff1024_nl20_smooth.pth',\
-                '../weights/weights_dm512_nh8_ff4096_nl4_smooth.pth', \
-                '../weights/weights_dm768_nh12_ff4608_nl2_smooth.pth']
+# weights_files = ['../weights/weights_dm256_nh4_ff1024_nl20_sched.pth',\
+#                 '../weights/weights_dm512_nh8_ff4096_nl4_sched.pth', \
+#                 '../weights/weights_dm768_nh12_ff4608_nl2_sched.pth']
+
+# weights_files = ['../weights/weights_dm256_nh4_ff1024_nl6_downs100_autoreg_epoch_32.pth',\
+#                 '../weights/weights_dm256_nh4_ff1024_nl6_downs75_autoreg_epoch_32.pth', \
+#                 '../weights/weights_dm256_nh4_ff1024_nl6_downs50_autoreg_epoch_26.pth',\
+#                 '../weights/weights_dm256_nh4_ff1024_nl6_downs40_autoreg_epoch_23.pth',\
+#                 '../weights/weights_dm256_nh4_ff1024_nl6_downs25_autoreg_epoch_32.pth']
+
+weights_files = ['../weights/weights_dm256_nh4_ff1024_nl6_downs50_new_epoch_32.pth']
 
 results = []
 
 for i, weight in enumerate(weights_files):
-    
-    print(i, weight)
-    
+# for i in range(1, 12):
+#     weight = f'../weights/weights_dm768_nh12_ff4608_nl2_tok_epoch_{i}.pth'
+#     print(i, weight)
+
     # Define the hyperparameters --> take them from the weights name
     d_model        = int(weight.split('_')[1].split('dm')[1])
     num_heads      = int(weight.split('_')[2].split('nh')[1])
@@ -37,7 +48,7 @@ for i, weight in enumerate(weights_files):
     model = MultiLayerTransformerDecoder(vocab_size, d_model, num_heads, ff_hidden_layer, dropout, num_layers)
     
     # Load the model weights
-    fabric = Fabric(accelerator='cuda', devices=1)
+    fabric = Fabric(accelerator='cuda', devices=[0])
     fabric.launch()
     state = {'model': model}
     fabric.load(weight, state)
@@ -47,11 +58,11 @@ for i, weight in enumerate(weights_files):
         
         generated_smiles, sucess_rate = generate(100, seq, model, fabric, max_length=80,\
                                                 temperature=1.2, verbose=False,\
-                                                outdir='test_generation_smooth',\
-                                                outname='generated_molecules_weights_%d_seq_%d.csv'%(i, j))
+                                                outdir='test_generation_new_dataset',\
+                                                outname='generated_molecules_weights_%d_seq_%d_expanded.csv'%(i, j))
         print(i, j, sucess_rate)
         
         results.append([i, weight, j, seq, sucess_rate])
         
 df_results = pd.DataFrame(results, columns=['weight_idx', 'weight', 'seq_idx', 'sequence', 'success_rate'])
-df_results.to_csv('test_generation_smooth/results_success_rate.csv', index=False)
+df_results.to_csv('test_generation_new_dataset/results_success_rate_dm256_nh4_ff1024_nl6.csv', index=False)
